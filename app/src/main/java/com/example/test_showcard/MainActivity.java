23c704721847d6a6b07562ea5f5e3710076bc8d5
15282807881 <1261729204@qq.com>
2021-04-16 14:36:04 +0800

    Messes

--- app/src/main/java/com/example/test_showcard/MainActivity.java
+++ app/src/main/java/com/example/test_showcard/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.test_showcard;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        JSONObject jsonObject;
         setContentView(R.layout.activity_main);
     }
 }
